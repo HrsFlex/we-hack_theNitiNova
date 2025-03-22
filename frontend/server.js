95436bbac83045b52ac9addfde0e90a2ef6bbb48
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-03-23 01:15:14 +0530

    updated code for server.js

--- frontend/server.js
+++ frontend/server.js
@@ -10,10 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin:"http://localhost:3000", credentials: true }));
 app.use(express.json());
 
-app.use("/api/pdf", pdfRoutes);
+app.use("/api/pdf",pdfRoutes);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
